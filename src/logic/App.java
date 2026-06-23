8762888dfb2a4174ee801070217f7eb4879f315c
BasemAlassaf <basemalassaf61@gmail.com>
2026-06-23 09:41:31 +0300

    the sorting function is ready and i move the get routes and get route func to the network NOTE: watch the parameters about the get functions

--- src/logic/App.java
+++ src/logic/App.java
@@ -1,5 +1,7 @@
 package logic;
 
+import java.util.HashMap;
+
 import ui.UI;
 
 public class App {
@@ -25,10 +27,31 @@ public class App {
         network.addRoute(1,2, 10);
         network.addRoute(2, 3, 100);
         network.addRoute(3, 1, 50);
-        
+        network.addRoute(4, 2, 50);
+        network.addRoute(4, 3, 50);
+        // network.getStation(1).addRoute(new Route(network.getStation(1), network.getStation(3), 20));
+        // network.getStation(1).addRoute(new Route(network.getStation(1), network.getStation(5), 15));
+        // network.getStation(2).addRoute(new Route(network.getStation(2), network.getStation(4), 30));
+        // network.getStation(2).addRoute(new Route(network.getStation(2), network.getStation(6), 25));
+        // network.getStation(3).addRoute(new Route(network.getStation(3), network.getStation(4), 12));
+        // network.getStation(3).addRoute(new Route(network.getStation(3), network.getStation(7), 35));
+        // network.getStation(7).addRoute(new Route(network.getStation(7), network.getStation(3), 1035));
+        // network.getStation(5).addRoute(new Route(network.getStation(5), network.getStation(6), 18));
+        // network.getStation(6).addRoute(new Route(network.getStation(6), network.getStation(5), 28));
+
         //new UI(network);
 
         System.out.println(network.hasCycle());
+        network.deleteStation(network.stations.get(1));
+        for (int s : network.stations.keySet()) {
+            System.out.println(s  + "   "  + network.stations.get(s) );
+        }
+        for (Station station : network.routes.keySet()) {
+            System.out.println( station.name  + "    " + network.routes.get(station).size());
+        }
 
+        for (Station station : network.getSortedStations()) {
+            System.out.println(station);
+        }
     }
 }
